e598437c07f296088f9c3905f1a74520a845962c
Anderson Gaitolini <gaitolini@gmail.com>
2024-03-10 17:23:30 -0300

    Update sensor.py

--- esphome/components/zmpt101b/sensor.py
+++ esphome/components/zmpt101b/sensor.py
@@ -47,7 +47,7 @@ CONFIG_SCHEMA = (
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(0)   #config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     cg.add_library('EmonLib', None)
